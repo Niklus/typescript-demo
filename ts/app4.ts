58ab45f2dde74e8c139aeb935f1b4b5bb2c9f694
Nicholas Otieno <niklasoti@gmail.com>
2022-04-09 09:19:24 +0200

    Put typescript files in ts folder

--- ts/app4.ts
+++ ts/app4.ts
@@ -11,10 +11,8 @@ async function getTodo(url: string): Promise<Todo> {
   return data;
 }
 
-const url: string = 'https://jsonplaceholder.typicode.com/todos/1';
-
-getTodo(url).then((todo: Todo) => {
-  const todoContainer: Node = document.createElement('pre');
+getTodo("https://jsonplaceholder.typicode.com/todos/1").then((todo: Todo) => {
+  const todoContainer: Node = document.createElement("pre");
   todoContainer.textContent = JSON.stringify(todo, null, 3);
   document.body.appendChild(todoContainer);
 });
